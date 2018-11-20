40c0c7c932d34022c19a0898252fd4d17b325dfd
open-trade <info@opentradesolutions.com>
2018-11-20 16:08:42 +0800

    readable soci cast error

--- src/opentrade/database.h
+++ src/opentrade/database.h
@@ -2,9 +2,13 @@
 #define OPENTRADE_DATABASE_H_
 
 #include <soci.h>
+#include <boost/type_index.hpp>
 #include <cstring>
 #include <memory>
 #include <string>
+#include <typeinfo>
+
+#include "logger.h"
 
 namespace opentrade {
 
@@ -15,9 +19,43 @@ class Database {
   static auto Session() { return std::make_unique<soci::session>(*pool_); }
 
   template <typename T>
+  static T Get(soci::row const& row, int index) {
+    try {
+      return row.get<T>(index);
+    } catch (const std::bad_cast& e) {
+      auto type = "unknown";
+      switch (row.get_properties(index).get_data_type()) {
+        case soci::dt_string:
+          type = "soci::dt_string";
+          break;
+        case soci::dt_date:
+          type = "soci::dt_date";
+          break;
+        case soci::dt_double:
+          type = "soci::dt_double";
+          break;
+        case soci::dt_integer:
+          type = "soci::dt_integer";
+          break;
+        case soci::dt_long_long:
+          type = "soci::dt_long_long";
+          break;
+        case soci::dt_unsigned_long_long:
+          type = "soci::dt_unsigned_long_long";
+          break;
+      }
+      LOG_ERROR("Failed to cast '"
+                << row.get_properties(index).get_name() << "', expected '"
+                << type << "' compatible, but you set '"
+                << boost::typeindex::type_id<T>().pretty_name() << "'");
+      throw e;
+    }
+  }
+
+  template <typename T>
   static T GetValue(soci::row const& row, int index, T default_value) {
     if (row.get_indicator(index) != soci::i_null)
-      return row.get<T>(index);
+      return Get<T>(row, index);
     else
       return default_value;
   }
@@ -25,7 +63,7 @@ class Database {
   static const char* GetValue(soci::row const& row, int index,
                               const char* default_value) {
     if (row.get_indicator(index) != soci::i_null)
-      return strdup(row.get<std::string>(index).c_str());
+      return strdup(Get<std::string>(row, index).c_str());
     else
       return default_value;
   }
