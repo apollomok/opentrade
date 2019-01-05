c887f5618d7bfcaa5dc8bbc70cc6bfab96297460
open-trade <info@opentradesolutions.com>
2019-01-05 16:56:33 +0800

    refactor target reply

--- src/opentrade/connection.cc
+++ src/opentrade/connection.cc
@@ -404,7 +404,14 @@ void Connection::OnMessageSync(const std::string& msg,
     } else if (action == "position") {
       OnPosition(j, msg);
     } else if (action == "target") {
-      OnTarget(j, msg);
+      if (j.size() == 1) {
+        for (auto& pair : AccountManager::Instance().sub_accounts_) {
+          if (!user_->is_admin && !user_->GetSubAccount(pair.first)) continue;
+          OnTarget(json{"target", pair.second->name}, "");
+        }
+      } else {
+        OnTarget(j, msg);
+      }
     } else if (action == "offline") {
       if (j.size() > 2) {
         auto seq_algo = Get<int64_t>(j[2]);
@@ -1083,7 +1090,6 @@ void Connection::OnLogin(const std::string& action, const json& j) {
     auto accs = user->sub_accounts();
     for (auto& pair : *accs) {
       Send(json{"sub_account", pair.first, pair.second->name});
-      if (!user->is_admin) OnTarget(json{"target", pair.second->name}, "");
     }
     if (user->is_admin) {
       for (auto& pair : AccountManager::Instance().users_) {
@@ -1092,9 +1098,6 @@ void Connection::OnLogin(const std::string& action, const json& j) {
           Send(json{"user_sub_account", pair.first, pair2.first,
                     pair2.second->name});
         }
-      }
-      for (auto& pair : AccountManager::Instance().sub_accounts_) {
-        OnTarget(json{"target", pair.second->name}, "");
       }
     }
     for (auto& pair : AccountManager::Instance().broker_accounts_) {
