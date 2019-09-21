5228665c321d697e590079fefb6ab542ef3fb8b7
open-trade <info@opentradesolutions.com>
2019-09-21 23:01:23 +0800

    roll pnl

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -5,4 +5,8 @@ mv ./store $d
 mv ./fixlog $d
 mv ./fixstore $d
 mv ./logs $d
+rootd=`pwd`
+cd $d/store
+mkdir store && ls pnl* | xargs -I file sh -c "tail -n 10000 file > store/file" && mv store $rootd
+cd -
 tar czf $d.tar.gz $d && /bin/rm -rf $d
