7f6777816a177a476cf845aad2c2fbf01f0ab7fc
open-trade <info@opentradesolutions.com>
2019-10-10 11:54:43 +0000

    prepare for centos8

---
+++ install_centos.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+yum install epel-release
+yum update
+yum install libpq-devel sqlite-devel git python3-devel make gcc cmake clang vim ctags tbb-devel wget dos2unix libtool apr-devel apr-util-devel valgrind gdb
+wget http://prdownloads.sourceforge.net/quickfix/quickfix-1.15.1.tar.gz
+tar xzf quickfix-1.15.1.tar.gz 
+cd quickfix/
+./bootstrap 
+make install
+wget https://www-eu.apache.org/dist/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz
+tar xzf apache-log4cxx-0.10.0.tar.gz 
+cd ~/apache-log4cxx-0.10.0
+./configure
+find . -name inputstreamreader.cpp | xargs sed -i 's/using namespace log4cxx;/using namespace log4cxx;\n#include <cstring>/g'
+find . -name socketoutputstream.cpp | xargs sed -i 's/using namespace log4cxx;/using namespace log4cxx;\n#include <cstring>/g'
+find . -name loggingevent.cpp | xargs sed -i 's/0x/(char)0x/g' 
+find . -name locationinfo.cpp | xargs sed -i 's/0x/(char)0x/g' 
+find . -name objectoutputstream.cpp | xargs sed -i 's/0x/(char)0x/g' 
+find . -name console.cpp | xargs sed -i 's/using namespace log4cxx;/using namespace log4cxx;\n#include <cstring>/g'
+find . -name domtestcase.cpp | xargs sed -i 's/0x/(char)0x/g' 
+make install
+wget https://github.com/SOCI/soci/archive/3.2.3.tar.gz
+tar xzf 3.2.3.tar.gz 
+cd soci-3.2.3/
+cmake src
+make install
+wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
+pip3 install numpy
+tar xzf boost_1_66_0.tar.gz
+cd boost_1_66_0
+cd /usr/include; ln -s python3.6m python3.6; cd -
+./bootstrap.sh --with-python-root=/usr/bin/python3
+ln -s /usr/bin/python3 python; ./b2 --with-python --debug-configuration install
